df5a7b27421556b04138d650003f55bfe04131f6
JAYAPRAKASH K <jayaprakash6034@gmail.com>
2019-11-22 21:22:48 -0500

    Mulitple query insertion

--- buildDatabase.py
+++ buildDatabase.py
@@ -6,6 +6,83 @@ import numpy as np
 import tableCreation
 
 
+def queryExecution(read_table, check_loop, check_union_or_prod):
+    # read_table= pd.read_sql_query(input_query,mydb)
+    print(read_table)
+    semantic_choice = input("choose the semantincs:\n 0 - Bag semantics\n 1 - Provenance semantics\n 2 - Probability semantics\n 3 - Uncertainity semantics\n 4 - Standard semantics\n " )
+    
+    new_annotation = []
+    if(semantic_choice == str(0)):   #bag semantics
+        if (check_loop == True or check_union_or_prod == "union"):
+            for i in read_table['Annotation']:
+                results = list(map(int, i.split(",")))
+                new_annotation.append(sum(results))    
+            read_table['Annotation'] = new_annotation
+        
+        elif check_union_or_prod == "product": 
+            read_table['Annotation'] = joinTuples(read_table)      
+                                        
+    if(semantic_choice == str(1)): #provenence semantics     
+        for i in read_table['Annotation']:
+            if(check_union_or_prod == "product"):
+                new_annotation.append(i.replace(",", " X "))
+            else: 
+                new_annotation.append(i.replace(",", " + "))
+        read_table['Annotation'] = new_annotation
+
+    if(semantic_choice == str(2)): #probability semantics
+        if (check_loop == True):
+            for i in read_table['Annotation']:
+                str_empty = i.split(",")
+                summ = 1;
+                for j in str_empty:
+                    summ = summ * (1 - float(j))
+                new_annotation.append(round((1 - summ) , 2))    
+            read_table['Annotation'] = new_annotation
+
+        else:
+            if check_union_or_prod == "union":
+                for i in read_table['Annotation']:
+                    results = list(map(float, i.split(",")))
+                    total = sum(results)        # a+b+c 
+                    product = np.prod(results)  # abc
+                    sumVal = results[0] * results[len(results) - 1]
+                    
+                    for j in range(len(results)):    # ab + bc + ac
+                        if (j < len(results)-1 ):
+                            sumVal = sumVal + (results[j] * results[j+1])
+                    
+                    new_annotation.append(round((total - product + sumVal),2))
+                read_table['Annotation'] = new_annotation
+            
+            elif check_union_or_prod == "product": 
+                read_table['Annotation'] = joinTuples(read_table)  
+                        
+    if(semantic_choice == str(3)):  #uncertainity semantics
+        if (check_loop == True or check_union_or_prod == "union"):
+            for i in read_table['Annotation']:
+                str_empty = i.split(",")
+                new_annotation.append(max(list(map(float, str_empty))))    
+            read_table['Annotation'] = new_annotation
+
+        elif check_union_or_prod == "product": 
+            read_table['Annotation'] = joinTuples(read_table)  
+
+    if(semantic_choice == str(4)):  #standard semantics
+        for i in read_table['Annotation']:
+            new_annotation.append(1)    
+        read_table['Annotation'] = new_annotation
+        
+    check_union_or_prod = ""  
+    check_loop = True  
+    print(read_table)    
+    return (read_table)
+
+
+    # print(list(read_table.columns))
+    # if(len(viewName) > 0):
+    #     mydb._execute_query("drop view "+viewName+"") 
+
 def joinTuples(table):
     new_annotation = []
     for i in table['Annotation']:
@@ -20,93 +97,35 @@ def readTable(mydb, mycursor):
     check_loop = True
     check_union_or_prod = ""
     viewName = ""
+    k = 0
     while True:
         input_query=input("Enter the relational algebra query: ")
-        if("create view" in input_query):
-            check_loop = False
-            viewName = input_query.split(" ")[2]   
-            mydb._execute_query(input_query)  
+        
+        if("#" in input_query):
+            queryList = input_query.split("#")
 
-            if("union" in input_query):
-                check_union_or_prod = "union"
-            else:
-                check_union_or_prod = "product"    
-
-            print(pd.read_sql_query("select * from "+viewName, mydb))
+            
+            for query in queryList:
+                if len(query) > 0:
+                    queue = pd.read_sql_query(query, mydb)
+                        
+                    if("union" in query or "join" in query):
+                        check_loop = False
+                        # viewName = input_query.split(" ")[2]   
+                        # mydb._execute_query(input_query)  
+                        if("union" in query):
+                            check_union_or_prod = "union"
+                        else:
+                            check_union_or_prod = "product"  
+                        
+                    updated_table = queryExecution(queue, check_loop, check_union_or_prod)    
+                    tableCreation.createTable(mydb, mycursor, updated_table, True, "T"+str(k))  
+   
+                k += 1;          
 
         else:
-            read_table= pd.read_sql_query(input_query,mydb)
-            print(read_table)
-            semantic_choice = input("choose the semantincs:\n 0 - Bag semantics\n 1 - Provenance semantics\n 2 - Probability semantics\n 3 - Uncertainity semantics\n 4 - Standard semantics\n " )
-            
-            new_annotation = []
-            if(semantic_choice == str(0)):   #bag semantics
-                if (check_loop == True or check_union_or_prod == "union"):
-                    for i in read_table['Annotation']:
-                        results = list(map(int, i.split(",")))
-                        new_annotation.append(sum(results))    
-                    read_table['Annotation'] = new_annotation
-                
-                elif check_union_or_prod == "product": 
-                    read_table['Annotation'] = joinTuples(read_table)      
-                                              
-            if(semantic_choice == str(1)): #provenence semantics            
-                for i in read_table['Annotation']:
-                    if(check_union_or_prod == "product"):
-                        new_annotation.append(i.replace(",", " X "))
-                    else: 
-                        new_annotation.append(i.replace(",", " + "))
-                read_table['Annotation'] = new_annotation
-
-            if(semantic_choice == str(2)): #probability semantics
-                if (check_loop == True):
-                    for i in read_table['Annotation']:
-                        str_empty = i.split(",")
-                        summ = 1;
-                        for j in str_empty:
-                            summ = summ * (1 - float(j))
-                        new_annotation.append(round((1 - summ) , 2))    
-                    read_table['Annotation'] = new_annotation
-
-                else:
-                    if check_union_or_prod == "union":
-                        for i in read_table['Annotation']:
-                            results = list(map(float, i.split(",")))
-                            total = sum(results)        # a+b+c 
-                            product = np.prod(results)  # abc
-                            sumVal = results[0] * results[len(results) - 1]
-                            
-                            for j in range(len(results)):    # ab + bc + ac
-                                if (j < len(results)-1 ):
-                                    sumVal = sumVal + (results[j] * results[j+1])
-                            
-                            new_annotation.append(round((total - product + sumVal),2))
-                        read_table['Annotation'] = new_annotation
-                    
-                    elif check_union_or_prod == "product": 
-                        read_table['Annotation'] = joinTuples(read_table)  
-                                
-            if(semantic_choice == str(3)):  #uncertainity semantics
-                if (check_loop == True or check_union_or_prod == "union"):
-                    for i in read_table['Annotation']:
-                        str_empty = i.split(",")
-                        new_annotation.append(max(list(map(float, str_empty))))    
-                    read_table['Annotation'] = new_annotation
-
-                elif check_union_or_prod == "product": 
-                    read_table['Annotation'] = joinTuples(read_table)  
- 
-            if(semantic_choice == str(4)):  #standard semantics
-                for i in read_table['Annotation']:
-                    new_annotation.append(1)    
-                read_table['Annotation'] = new_annotation
-                
-            check_union_or_prod = ""  
-            check_loop = True      
-            print(read_table)
-
-            if(len(viewName) > 0):
-                mydb._execute_query("drop view "+viewName+"") 
+            updated_table = queryExecution(queue, check_loop, check_union_or_prod)
+                                                
         
         input_question = input("Do you want to continue yes/no: ")
         if(input_question.upper() == "NO"):
@@ -120,5 +139,8 @@ database="mydatabase"
 )
 
 mycursor = mydb.cursor()          
-tableCreation.createTable(mydb, mycursor)            
+f = open('test.csv', 'r')
+data = pd.read_csv(f)
+tableCreation.createTable(mydb, mycursor, data, False, "krish_table")            
 readTable(mydb, mycursor)
+f.close()
--- tableCreation.py
+++ tableCreation.py
@@ -4,127 +4,88 @@ import mysql.connector
 import csv, ast
 # import buildDatabase
 
-def createTable(mydb, mycursor):
-    f = open('test.csv', 'r')
-    reader = csv.reader(f)
-    data = pd.read_csv(f)
+def multiple_table_insertion(mydb,mycursor,data2,tableName):
+    cols = "`,`".join([str(i) for i in data2.columns.tolist()])
+    for i,row in data2.iterrows():
+        sql = "INSERT INTO " +tableName+ "(`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        mycursor.execute(sql, tuple(row))
+        mydb.commit() 
 
-    # print(random.randint(0, 1))
-    longest, headers, type_list = [], [], []
-            
-    def dataType(val, current_type):
-        try:
-            # Evaluates numbers to an appropriate type, and strings an error
-            t = ast.literal_eval(val)
-        except ValueError:
-            return 'varchar'
-        except SyntaxError:
-            return 'varchar'
-            return 'varchar'
-
-    for row in reader:
-        if len(headers) == 0:
-            headers = row
-            for col in row:
-                longest.append(0)
-                type_list.append('')
-        else:
-            for i in range(len(row)):
-                # NA is the csv null value
-                if type_list[i] == 'varchar' or row[i] == 'NA':
-                    pass
-                else:
-                    var_type = dataType(row[i], type_list[i])
-                    type_list[i] = 'varchar'
-            if len(row[i]) > longest[i]:
-                longest[i] = len(row[i])
-    f.close()
-
-    for itr in range(5):
-        tableName = "green_paper_table"+str(itr)
-        statement = "create table "+tableName+"("
-        for i in range(len(headers)):
-            if type_list[i] == 'varchar':
-                statement = (statement + '\n{} varchar(255),').format(headers[i].lower(), str(longest[i]))
-            else:
-                statement = (statement + '\n' + '{} {}' + ',').format(headers[i].lower(), type_list[i])
-        statement = statement + '\n' + "Annotation " + 'varchar(255),'
+def createTable(mydb, mycursor, data, multiTable, myTabName):    
+    if(multiTable == True):
+        tableName = myTabName
+        statement = "create table "+tableName+" ("
+        for i in data.columns:
+            statement = (statement + '\n{} varchar(255),').format(i.lower())
+        # statement = statement + '\n' + "Annotation " + 'varchar(255),'
         statement = statement[:-1] + ");"
 
-        # print(statement)
-        # mycursor.execute(statement)
-        # mydb.commit()
+        print(statement)
+        mycursor.execute(statement)
+        mydb.commit()
 
-        # set columns of headers in the table
-        head = ""
-        for lis in headers:
-            if len(head) > 0:
-                head = head +', '+ lis
-            else:
-                head = lis
-        head = head +', '+ "Annotation"
+        multiple_table_insertion(mydb,mycursor,data,tableName)
 
-        # print(head)
-        # set columns for the values in the table
-        rowList =""        
-        for x in range(len(headers)):
-            if len(rowList) > 0:
-                rowList = rowList +', '+ "%s"
-            else:
-                rowList = "%s"  
+    else:    
+        for itr in range(5):
+            tableName = myTabName+str(itr)
+            statement = "create table "+tableName+"("
+            for i in data.columns:
+                statement = (statement + '\n{} varchar(255),').format(i.lower())
+            statement = statement + '\n' + "Annotation " + 'varchar(255),'
+            statement = statement[:-1] + ");"
 
-        if(itr == 3):
-            rowList = rowList +', '+ "%s"
-        else:
-            rowList = rowList +', '+ "%d"
+            # print(statement)
+            # mycursor.execute(statement)
+            # mydb.commit()
 
-        
-        # data = pd.read_csv(f)
+            # bag semantics 
+            if(itr == 0):
+                data3=data.groupby(data.columns.tolist(),as_index=False).size().reset_index(name='Annotation')
+                print(data3)
+                # cols = "`,`".join([str(i) for i in data3.columns.tolist()])
+                # for i,row in data3.iterrows():
+                #     sql = "INSERT INTO " +tableName+" (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+                #     mycursor.execute(sql, tuple(row))
+                #     mydb.commit()
 
-        # bag semantics 
-        if(itr == 0):
-            data3=data.groupby(data.columns.tolist(),as_index=False).size().reset_index(name='Annotation')
-            print(data3)
-            # cols = "`,`".join([str(i) for i in data3.columns.tolist()])
-            # for i,row in data3.iterrows():
-            #     sql = "INSERT INTO " +tableName+" (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-            #     mycursor.execute(sql, tuple(row))
-            #     mydb.commit()
+            duplicate_remov_dataFrame = pd.DataFrame(data.drop_duplicates()) 
+            semantaincs_arr = []
+            
+            if(itr == 1):# provenence semantics
+                for x in range(duplicate_remov_dataFrame.shape[0]):
+                    semantaincs_arr.append("t"+str(x))
+                duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
+                print(duplicate_remov_dataFrame)    
+            
+            if(itr == 2):# probabilty semantics
+                for x in range(duplicate_remov_dataFrame.shape[0]):
+                    semantaincs_arr.append(round(random.uniform(0.0,1.0), 2))
+                duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
+                print(duplicate_remov_dataFrame)
+            
+            if(itr == 3):# certainity semantics
+                for x in range(duplicate_remov_dataFrame.shape[0]):
+                    semantaincs_arr.append(round(random.uniform(0.0,1.0), 2))
+                duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
+                print(duplicate_remov_dataFrame)
 
-        duplicate_remov_dataFrame = pd.DataFrame(data.drop_duplicates()) 
-        semantaincs_arr = []
-        
-        if(itr == 1):# provenence semantics
-            for x in range(duplicate_remov_dataFrame.shape[0]):
-                semantaincs_arr.append("t"+str(x))
-            duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
-            print(duplicate_remov_dataFrame)    
-        
-        if(itr == 2):# probabilty semantics
-            for x in range(duplicate_remov_dataFrame.shape[0]):
-                semantaincs_arr.append(round(random.uniform(0.0,1.0), 2))
-            duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
-            print(duplicate_remov_dataFrame)
-        
-        if(itr == 3):# certainity semantics
-            for x in range(duplicate_remov_dataFrame.shape[0]):
-                semantaincs_arr.append(round(random.uniform(0.0,1.0), 2))
-            duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
-            print(duplicate_remov_dataFrame)
+            if(itr == 4):# standard semantics
+                for x in range(duplicate_remov_dataFrame.shape[0]):
+                    semantaincs_arr.append(1)
+                duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
+                print(duplicate_remov_dataFrame)
 
-        if(itr == 4):# standard semantics
-            for x in range(duplicate_remov_dataFrame.shape[0]):
-                semantaincs_arr.append(1)
-            duplicate_remov_dataFrame['Annotation'] = semantaincs_arr
-            print(duplicate_remov_dataFrame)
+            if(itr > 0):   
+                # data2 = data.drop_duplicates()
+                
+                multiple_table_insertion(mydb,mycursor,data,tableName)
+                
+                # cols = "`,`".join([str(i) for i in duplicate_remov_dataFrame.columns.tolist()])
+                # for i,row in duplicate_remov_dataFrame.iterrows():
+                #     sql = "INSERT INTO " +tableName+ "(`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+                #     mycursor.execute(sql, tuple(row))
+                #     mydb.commit()             
+                # print(data2)
 
-        # if(itr > 0):   
-        #     data2 = data.drop_duplicates()
-        #     cols = "`,`".join([str(i) for i in duplicate_remov_dataFrame.columns.tolist()])
-        #     for i,row in duplicate_remov_dataFrame.iterrows():
-        #         sql = "INSERT INTO " +tableName+ "(`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-        #         mycursor.execute(sql, tuple(row))
-        #         mydb.commit()             
-        #     print(data2)
-
-   
+    
