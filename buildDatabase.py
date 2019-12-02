805095c478b1911722c5601b7ac89d85f88fc8a2
JAYAPRAKASH K <jayaprakash6034@gmail.com>
2019-12-02 17:39:12 -0500

    final refactor

--- buildDatabase.py
+++ buildDatabase.py
@@ -8,7 +8,6 @@ import sqlite3
 from sqlalchemy import create_engine
 import datetime
 
-
 def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice):
     # print(read_table)    
     datas = []
@@ -18,7 +17,6 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
             header= read_table.columns.tolist()            
             header.remove('annotation')
             datas=read_table.groupby(header)['annotation'].agg([('annotation', ', '.join)]).reset_index()
-            # print(datas)    
             for i in datas['annotation']:
                 results = list(map(int, i.split(",")))
                 new_annotation.append(sum(results))
@@ -40,7 +38,6 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
         if(check_union_or_prod == "product"):
             read_table['Annotation']=read_table['Annotation'] +" X "+ read_table['Annotation1']
             read_table.drop('Annotation1', axis=1, inplace=True)
-            # new_annotation.append(i.replace(",", " X "))
             datas = read_table
         else: 
             header= read_table.columns.tolist()
@@ -52,7 +49,6 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
             header= read_table.columns.tolist()
             header.remove('annotation')
             datas=read_table.groupby(header)['annotation'].agg([('annotation', ', '.join)]).reset_index()
-            # print(datas)         
             for i in datas['annotation']:
                 str_empty = i.split(",")
                 summ = 1
@@ -68,7 +64,6 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
                 header= read_table.columns.tolist()
                 header.remove('Annotation')
                 datas=read_table.groupby(header)['Annotation'].agg([('Annotation', ', '.join)]).reset_index()
-                # print(datas)
                 for i in datas['Annotation']:
                     str_empty = i.split(",")
                     summ = 1;
@@ -76,21 +71,18 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
                         summ = summ * (1 - float(j))
                     new_annotation.append(round((1 - summ) , 2))    
                 datas['Annotation'] = new_annotation
-                # read_table['Annotation'] = read_table.apply(lambda row: (float(row['Annotation'])*float(row['Annotation1'])),axis=1)  
                         
     if(semantic_choice == str(3)):  #uncertainity semantics
         if (check_loop == True or check_union_or_prod == "union"):
             header= read_table.columns.tolist()            
             header.remove('annotation')
             datas=read_table.groupby(header)['annotation'].agg([('annotation', ', '.join)]).reset_index()
-            # print(datas)            
             for i in datas['annotation']:               
                 str_empty = i.split(",")
                 new_annotation.append(max(list(map(float, str_empty))))    
             datas['annotation'] = new_annotation
 
         elif check_union_or_prod == "product": 
-            # read_table['Annotation'] = joinTuples(read_table)
             read_table['Annotation'] = read_table.apply(lambda row: (str(float(row['Annotation'])*float(row['Annotation1']))),axis=1)
             read_table.drop('Annotation1', axis=1, inplace=True)
             header= read_table.columns.tolist()
@@ -121,8 +113,8 @@ def queryExecution(read_table, check_loop, check_union_or_prod, semantic_choice)
                 results = list(map(int, i.split(",")))
                 new_annotation.append(np.prod(results))
             datas['Annotation'] =new_annotation
-    
-    # print(datas)                     
+
+    print(datas)
     return (datas)
        
 def readTable(mydb, mycursor):
@@ -133,15 +125,18 @@ def readTable(mydb, mycursor):
     while True:
         input_query=input("Enter the relational algebra query: ")
         semantic_choice = input("choose the semantincs:\n 0 - Bag semantics\n 1 - Provenance semantics\n 2 - Probability semantics\n 3 - Uncertainity semantics\n 4 - Standard semantics\n " )
-        start_time = datetime.datetime.now()
+        startTime = datetime.datetime.now()
+            
         if("#" in input_query):
             queryList = input_query.split("#")
 
-            
+            LastInLoop=False;
             for query in queryList:
                 if len(query) > 0:
+                    LastInLoop=False;
                     queue = pd.read_sql_query(query, mydb)
-                        
+                    if(query==queryList[-1]):
+                        LastInLoop=True;
                     if("union" in query or "join" in query):
                         check_loop = False  
                         if("union" in query):
@@ -150,16 +145,14 @@ def readTable(mydb, mycursor):
                             check_union_or_prod = "product"  
                        
                     updated_table = queryExecution(queue, check_loop, check_union_or_prod, semantic_choice)    
-                    print(updated_table)
-                    tableCreation.createTable(engine,mydb, mycursor, updated_table, True, "t"+str(k))  
+                    tableCreation.createTable(engine,mydb, mycursor, updated_table, True, "t"+str(k),LastInLoop)  
                     check_union_or_prod=""
                     check_loop=True
-                k += 1;
+                k += 1
 
             # to drop the tables
-            for tableNo in range(k):                  
+            for tableNo in range(k-1):                  
                 tName = "t"+str(tableNo)
-                # print(tName)
                 mycursor.execute("DROP TABLE "+tName+"", mydb)
             k = 0
 
@@ -172,13 +165,13 @@ def readTable(mydb, mycursor):
                     check_union_or_prod = "product"    
             queue = pd.read_sql_query(input_query, mydb)                        
             updated_table = queryExecution(queue, check_loop, check_union_or_prod,semantic_choice)
-            print(updated_table)
+            # print(updated_table)
                                                 
-        end_time = datetime.datetime.now()
-        total_time = end_time - start_time
-
-        print("Execution Time in seconds: ",total_time.seconds)
+        endTime = datetime.datetime.now()
+        totalTime = endTime - startTime
+        print("Execution time in seconds", totalTime.seconds)       
         input_question = input("Do you want to continue yes/no: ")
+        
         if(input_question.upper() == "NO"):
             break
     
@@ -191,9 +184,9 @@ database="mydatabase"
 
 mycursor = mydb.cursor()
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",pw="",db="mydatabase"))          
-f = open('test.csv', 'r')
+f = open('Sales.orders.csv', 'r')
 data = pd.read_csv(f)
 data.columns=data.columns.str.replace(' ', '')
-tableCreation.createTable(engine,mydb, mycursor, data, False, "sales_staff")            
+tableCreation.createTable(engine,mydb, mycursor, data, False, "sales_orders",False)            
 readTable(mydb, mycursor)
 f.close()
--- tableCreation.py
+++ tableCreation.py
@@ -4,29 +4,27 @@ import mysql.connector
 import csv, ast
 
 
-def multiple_table_insertion(engine,mydb,mycursor,data2,tableName):
+def multiple_table_insertion(engine,mydb,mycursor,data2,tableName,LastInLoop):
     cols = "`,`".join([str(i) for i in data2.columns.tolist()])
-    datadb=data2.values.tolist()
-    data2.to_sql(tableName, con = engine, if_exists = 'append', index=False)
-    # for i,row in data2.iterrows():
-    #     sql = "INSERT INTO " +tableName+ "(`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-    #     mycursor.execute(sql, tuple(row))
-    #     mydb.commit() 
-
-def createTable(engine,mydb, mycursor, data, multiTable, myTabName):    
+    if(LastInLoop==True):
+        data2.to_csv('test'+tableName+'.csv', encoding='utf-8', index=False)
+    else:    
+        data2.to_sql(tableName, con = engine, if_exists = 'append', index=False)
+   
+def createTable(engine,mydb, mycursor, data, multiTable, myTabName,LastInLoop):    
+    tableName = myTabName
     if(multiTable == True):
-        tableName = myTabName
-        statement = "create table "+tableName+" ("
-        for i in data.columns:
-            statement = (statement + '\n{} varchar(255),').format(i.lower())
-        # statement = statement + '\n' + "Annotation " + 'varchar(255),'
-        statement = statement[:-1] + ");"
+        if(LastInLoop==False):            
+            statement = "create table "+tableName+" ("
+            for i in data.columns:
+                statement = (statement + '\n{} varchar(65535),').format(i.lower())
+            statement = statement[:-1] + ");"
 
-        # print(statement)
-        mycursor.execute(statement)
-        mydb.commit()
+            # print(statement)
+            mycursor.execute(statement)
+            mydb.commit()
 
-        multiple_table_insertion(engine,mydb,mycursor,data,tableName)
+        multiple_table_insertion(engine,mydb,mycursor,data,tableName,LastInLoop)
 
     else:    
         for itr in range(5):
@@ -37,9 +35,9 @@ def createTable(engine,mydb, mycursor, data, multiTable, myTabName):
             statement = statement + '\n' + "annotation " + 'varchar(255),'
             statement = statement[:-1] + ");"
 
-            # prin
-            # mycursor.execute(statement)
-            # mydb.commit()t(statement)
+            # print(statement)
+            mycursor.execute(statement)
+            mydb.commit()
             
             duplicate_remov_dataFrame = pd.DataFrame(data.drop_duplicates()) 
             semantaincs_arr = []            
@@ -49,17 +47,6 @@ def createTable(engine,mydb, mycursor, data, multiTable, myTabName):
                 for x in range(duplicate_remov_dataFrame.shape[0]):
                     semantaincs_arr.append(str(int(random.uniform(1,10))))
                 duplicate_remov_dataFrame['annotation'] = semantaincs_arr
-                # print(duplicate_remov_dataFrame)
-
-
-                # data3=data.groupby(data.columns.tolist(),as_index=False).size().reset_index(name='Annotation')
-                # print(data3)
-                # cols = "`,`".join([str(i) for i in data3.columns.tolist()])
-                # for i,row in data3.iterrows():
-                #     sql = "INSERT INTO " +tableName+" (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-                #     mycursor.execute(sql, tuple(row))
-                #     mydb.commit()
-
             
             if(itr == 1):# provenence semantics
                 for x in range(duplicate_remov_dataFrame.shape[0]):
@@ -81,8 +68,7 @@ def createTable(engine,mydb, mycursor, data, multiTable, myTabName):
                     semantaincs_arr.append(str(1))
                 duplicate_remov_dataFrame['annotation'] = semantaincs_arr
 
-            # if(itr >= 0):  
-            #     multiple_table_insertion(engine,mydb,mycursor,duplicate_remov_dataFrame,tableName)
+            multiple_table_insertion(engine,mydb,mycursor,duplicate_remov_dataFrame,tableName,LastInLoop)
                 
 
     
